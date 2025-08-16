bf93f8b7e66e6ed5add623ea840053c628d5901a
Alkis <alkis.k1972@gmail.com>
2025-08-16 14:40:20 +0300

    Added Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,27 @@
-export default function Form() {
+import { useState } from 'react';
+
+export default function Form(storeNewItem) {
+    const [dateEntry, saveDateEntry] = useState('');
+    const [textEntry, saveTextEntry] = useState('');
     return (
         <div className='mtForm'>
-            <input type='text' />
-            <textarea></textarea>
-            <button>Add Fact</button>
+            <input
+                className='mtfield mtInput'
+                type='text'
+                value={dateEntry}
+                onChange={(e) => saveDateEntry(e.target.value)}
+            />
+            <textarea
+                className='mtfield mtArea'
+                value={textEntry}
+                onChange={(e) => saveTextEntry(e.target.value)}
+            />
+            <button
+                className='mtButton'
+                onClick={storeNewItem.storeItem(dateEntry, textEntry)}
+            >
+                Add Fact
+            </button>
         </div>
     );
 }
