bee0f8794a111eab2e11fb1c2047d21f8e0498f3
Alkis <alkis.k1972@gmail.com>
2025-08-16 11:49:20 +0300

    Initial commit

---
+++ src/components/Form.js
@@ -0,0 +1,9 @@
+export default function Form() {
+    return (
+        <div className='mtForm'>
+            <input type='text' />
+            <textarea></textarea>
+            <button>Add Fact</button>
+        </div>
+    );
+}
